efc92d8af6771f5cedcf05ffda9de3609d155a7a
Clancey <james.clancey@gmail.com>
2013-07-10 10:41:25 -0700

    Added the android project

---
+++ MonkeyBox.Droid/MonkeyView.cs
@@ -0,0 +1,14 @@
+using System;
+using Android.Views;
+using Android.Content;
+
+namespace MonkeyBox.Droid
+{
+	public class MonkeyView : SurfaceView
+	{
+		public MonkeyView (Context context) : base(context)
+		{
+		}
+	}
+}
+
